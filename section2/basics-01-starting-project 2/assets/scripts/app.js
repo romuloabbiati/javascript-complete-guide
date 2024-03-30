c6fb3a70585c26d058bf98cb05ec19a90a0686a4
romuloabbiati <rhabbiati@hotmail.com>
2024-03-30 09:58:33 +0000

    s2 returning values

--- section2/basics-01-starting-project 2/assets/scripts/app.js
+++ section2/basics-01-starting-project 2/assets/scripts/app.js
@@ -1,1 +1,13 @@
-alert('This works!');
+const defaultResult = 0
+let currentResult = defaultResult;
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result;
+};
+
+currentResult = add(1, 2);
+
+let calculationDescription = `(${defaultResult} + 10) * (3 / 2) - 1`;
+
+outputResult(currentResult, calculationDescription);
