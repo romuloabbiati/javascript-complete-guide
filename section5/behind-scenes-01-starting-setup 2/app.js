fd27c99db566b9a6d9a6561eec83add4da9b6986
romuloabbiati <rhabbiati@hotmail.com>
2024-04-20 20:10:06 +0100

    s5 l113 var vs let and const - introducing block scope

---
+++ section5/behind-scenes-01-starting-setup 2/app.js
@@ -0,0 +1,17 @@
+let name = 'Romulo';
+let hobbies;
+
+if (name === 'Romulo') {
+    hobbies = ['Cooking', 'coding'];
+    console.log(hobbies);
+}
+
+function greet() {
+    let age = 37;
+    let name = 'Romulo Hipolito Abbiati';
+    console.log(name, age);
+}
+
+console.log(name, hobbies);
+
+greet();
