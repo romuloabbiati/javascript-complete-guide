265d26c9d66b66caab5178fa87e39d44b2373261
romuloabbiati <rhabbiati@hotmail.com>
2024-04-23 08:16:44 +0100

    s6 l126 functions vs methods

---
+++ section6/functions-01-starting-code 2/app.js
@@ -0,0 +1,17 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+    console.log('game is starting...');
+}
+
+// const person = {
+//     name: 'Romulo', // a variable inside of an object is called PROPERTY
+//     greet: function() { // a function inside of an object is called METHOD
+//         console.log("Hello, I'm Romulo!");
+//     }
+// }
+
+// person.greet();
+
+
+startGameBtn.addEventListener('click', startGame);
