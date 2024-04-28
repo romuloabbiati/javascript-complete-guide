04238abea83f48700c5d7fb90822125fcd62b1a2
romuloabbiati <rhabbiati@hotmail.com>
2024-04-28 11:42:07 +0100

    s6 l141 adding bind to the calculator project

--- section2/basics-01-starting-project 2/assets/scripts/app.js
+++ section2/basics-01-starting-project 2/assets/scripts/app.js
@@ -58,23 +58,27 @@ function calculateResult(calculationType) {
   writeToLog(calculationType, initialResult, enteredNumber, currentResult);
 }
 
-function add() {
-  calculateResult("ADD");
+// function add() {
+//   calculateResult("ADD");
+// }
+
+// function subtract() {
+//   calculateResult("SUBTRACT");
+// }
+
+// function multiply() {
+//   calculateResult("MULTIPLY");
+// }
+
+// function divide() {
+//   calculateResult("DIVIDE");
+// }
+
+function calculate(operation) {
+  calculateResult(operation);
 }
 
-function subtract() {
-  calculateResult("SUBTRACT");
-}
-
-function multiply() {
-  calculateResult("MULTIPLY");
-}
-
-function divide() {
-  calculateResult("DIVIDE");
-}
-
-addBtn.addEventListener("click", add);
-subtractBtn.addEventListener("click", subtract);
-multiplyBtn.addEventListener("click", multiply);
-divideBtn.addEventListener("click", divide);
+addBtn.addEventListener("click", calculate.bind(this, 'ADD'));
+subtractBtn.addEventListener("click", calculate.bind(this, 'SUBTRACT'));
+multiplyBtn.addEventListener("click", calculate.bind(this, 'MULTIPLY'));
+divideBtn.addEventListener("click", calculate.bind(this, 'DIVIDE'));
