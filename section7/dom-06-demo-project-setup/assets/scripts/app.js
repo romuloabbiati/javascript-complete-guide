a0f2183cf49fec411064c3f9abdfb77ab52e9cb3
romuloabbiati <rhabbiati@hotmail.com>
2024-05-06 12:27:24 +0100

    s7 l173 controlling the backdrop

--- section7/dom-06-demo-project-setup/assets/scripts/app.js
+++ section7/dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,4 +1,29 @@
 const addMovieModal = document.getElementById('add-modal');
+
+const backdrop = document.getElementById('backdrop');
 // const addMovieModal = document.querySelector('#add-modal');
 // const addMovieModal = document.body.children[1];
 const startAddMovieButton = document.querySelector('header button');
+
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+
+const toggleMovieModal = () => { // function() {}
+    addMovieModal.classList.toggle('visible');
+    toggleBackgrop();
+};
+
+const toggleBackgrop = () => {
+    backdrop.classList.toggle('visible');
+};
+
+const backdropClickHandler = () => {
+    toggleMovieModal();
+};
+
+const cancelAddMovie = () => {
+    toggleMovieModal();
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
