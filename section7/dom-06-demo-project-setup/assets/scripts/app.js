a7c9b28ed7f1e0e639ca32142bb55351ba2d7850
romuloabbiati <rhabbiati@hotmail.com>
2024-05-06 17:37:02 +0100

    s7 l174 fetching and validating user input

--- section7/dom-06-demo-project-setup/assets/scripts/app.js
+++ section7/dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,29 +1,50 @@
-const addMovieModal = document.getElementById('add-modal');
+const addMovieModal = document.getElementById("add-modal");
 
-const backdrop = document.getElementById('backdrop');
+const backdrop = document.getElementById("backdrop");
 // const addMovieModal = document.querySelector('#add-modal');
 // const addMovieModal = document.body.children[1];
-const startAddMovieButton = document.querySelector('header button');
+const startAddMovieButton = document.querySelector("header button");
 
-const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
 
-const toggleMovieModal = () => { // function() {}
-    addMovieModal.classList.toggle('visible');
-    toggleBackgrop();
+const toggleMovieModal = () => {
+  // function() {}
+  addMovieModal.classList.toggle("visible");
+  toggleBackgrop();
 };
 
 const toggleBackgrop = () => {
-    backdrop.classList.toggle('visible');
+  backdrop.classList.toggle("visible");
 };
 
 const backdropClickHandler = () => {
-    toggleMovieModal();
+  toggleMovieModal();
 };
 
-const cancelAddMovie = () => {
-    toggleMovieModal();
+const cancelAddMovieHandler = () => {
+  toggleMovieModal();
 };
 
-startAddMovieButton.addEventListener('click', toggleMovieModal);
-backdrop.addEventListener('click', backdropClickHandler);
-cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imgUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imgUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert('Please enter valid values (rating between 1 and 5).');
+    return;
+  }
+};
+
+startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
