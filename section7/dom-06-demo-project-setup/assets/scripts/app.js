87b8eb5dd22e646ba88e9cae4cf8016370be7296
romuloabbiati <rhabbiati@hotmail.com>
2024-05-07 08:09:42 +0100

    s7 l176 Rendering movie items on the screen

--- section7/dom-06-demo-project-setup/assets/scripts/app.js
+++ section7/dom-06-demo-project-setup/assets/scripts/app.js
@@ -8,6 +8,39 @@ const startAddMovieButton = document.querySelector("header button");
 const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll("input");
+const entryTextSection = document.getElementById('entry-text');
+
+let movies = [];
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement('li');
+  newMovieElement.className = 'movie-element';
+  newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+      <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+      <h2>${title}</h2>
+      <p>${rating}/5 stars</p>
+    </div>
+  `
+  const listRoot = document.getElementById('movie-list');
+  listRoot.append(newMovieElement);
+};
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = 'block';
+  } else {
+    entryTextSection.style.display = 'none';
+  }
+};
+
+const clearMovieInput = () => {
+    for (let userInput of userInputs) {
+        userInput.value = '';
+    }
+};
 
 const toggleMovieModal = () => {
   // function() {}
@@ -25,6 +58,7 @@ const backdropClickHandler = () => {
 
 const cancelAddMovieHandler = () => {
   toggleMovieModal();
+  clearMovieInput();
 };
 
 const addMovieHandler = () => {
@@ -42,6 +76,19 @@ const addMovieHandler = () => {
     alert('Please enter valid values (rating between 1 and 5).');
     return;
   }
+
+  const newMovie = {
+    title: titleValue,
+    image: imgUrlValue,
+    rating: ratingValue
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieInput();
+  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  updateUI();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
