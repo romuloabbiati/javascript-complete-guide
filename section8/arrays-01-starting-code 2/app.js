a12f768e4ae7d667cd8b50699d3a4819eb3b4cf2
romuloabbiati <rhabbiati@hotmail.com>
2024-05-13 21:03:35 +0100

    s8 l186 which data can you store in arrays

--- section8/arrays-01-starting-code 2/app.js
+++ section8/arrays-01-starting-code 2/app.js
@@ -13,3 +13,16 @@ console.log(listItems);
 
 const arrayListItems = Array.from(listItems);
 console.log(arrayListItems);
+
+const hobbies = ['Coding', 'Cooking', 'Studying'];
+const personalData = [37, 'Romulo', {moreDetail: []}];
+
+const analyticsData = [[1, 1.6], [5.4, 2.1]];
+
+for (const data of analyticsData) {
+    for (const dataPoint of data) {
+        console.log(dataPoint);
+    }
+}
+
+console.log(personalData[1]);
