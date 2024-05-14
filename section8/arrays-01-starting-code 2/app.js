7f9ada27ec429c97f1d668718ecde5410a8d0263
romuloabbiati <rhabbiati@hotmail.com>
2024-05-14 06:46:22 +0100

    s8 l187 push pop unshift shift - adding and removing elements

--- section8/arrays-01-starting-code 2/app.js
+++ section8/arrays-01-starting-code 2/app.js
@@ -1,28 +1,39 @@
-const numbers = [1, 2, 3];
-console.log(numbers);
+// const numbers = [1, 2, 3];
+// console.log(numbers);
 
-// const moreNumbers = Array(5);
-// const moreNumber = new Array(5);
-// console.log(moreNumbers);
+// // const moreNumbers = Array(5);
+// // const moreNumber = new Array(5);
+// // console.log(moreNumbers);
 
-// const yetMoreNumbers = Array.of(1, 2);
-// console.log(yetMoreNumbers);
+// // const yetMoreNumbers = Array.of(1, 2);
+// // console.log(yetMoreNumbers);
 
-const listItems = document.querySelectorAll('li');
-console.log(listItems);
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
 
-const arrayListItems = Array.from(listItems);
-console.log(arrayListItems);
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems);
 
-const hobbies = ['Coding', 'Cooking', 'Studying'];
-const personalData = [37, 'Romulo', {moreDetail: []}];
+// const hobbies = ['Coding', 'Cooking', 'Studying'];
+// const personalData = [37, 'Romulo', {moreDetail: []}];
 
-const analyticsData = [[1, 1.6], [5.4, 2.1]];
+// const analyticsData = [[1, 1.6], [5.4, 2.1]];
 
-for (const data of analyticsData) {
-    for (const dataPoint of data) {
-        console.log(dataPoint);
-    }
-}
+// for (const data of analyticsData) {
+//     for (const dataPoint of data) {
+//         console.log(dataPoint);
+//     }
+// }
 
-console.log(personalData[1]);
+// console.log(personalData[1]);
+
+const hobbies = ['Sports', 'Cooking'];
+hobbies.push('Reading');
+hobbies.unshift('Coding');
+const poppedValue = hobbies.pop();
+hobbies.shift();
+console.log(hobbies);
+
+hobbies[1] = 'Coding';
+// hobbies[5] = 'Reading'; // rarely used
+console.log(hobbies);
