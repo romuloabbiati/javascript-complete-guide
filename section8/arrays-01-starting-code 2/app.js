e78323ae69239cbc88894bbd9d5bf6abb704a9c0
romuloabbiati <rhabbiati@hotmail.com>
2024-05-14 07:27:33 +0100

    s8 l189 selecting ranges & creating copies with slice

--- section8/arrays-01-starting-code 2/app.js
+++ section8/arrays-01-starting-code 2/app.js
@@ -27,19 +27,25 @@
 
 // console.log(personalData[1]);
 
-const hobbies = ['Sports', 'Cooking'];
-hobbies.push('Reading');
-hobbies.unshift('Coding');
-const poppedValue = hobbies.pop();
-hobbies.shift();
-console.log(hobbies);
+// const hobbies = ['Sports', 'Cooking'];
+// hobbies.push('Reading');
+// hobbies.unshift('Coding');
+// const poppedValue = hobbies.pop();
+// hobbies.shift();
+// console.log(hobbies);
 
-hobbies[1] = 'Coding';
-// hobbies[5] = 'Reading'; // rarely used
-console.log(hobbies, hobbies[4]);
+// hobbies[1] = 'Coding';
+// // hobbies[5] = 'Reading'; // rarely used
+// console.log(hobbies, hobbies[4]);
 
-hobbies.splice(1, 0, 'Good Food');
-console.log(hobbies);
+// hobbies.splice(1, 0, 'Good Food');
+// console.log(hobbies);
 
-const removedElements = hobbies.splice(-2, 2);
-console.log(hobbies);
+// const removedElements = hobbies.splice(-2, 2);
+// console.log(hobbies);
+
+const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+const storedResults = testResults.slice(2);
+testResults.push(5.91);
+
+console.log(testResults, storedResults);
