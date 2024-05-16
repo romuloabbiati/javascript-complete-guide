06034ef438690b4c4f8cebeda9dd6de693c25f23
romuloabbiati <rhabbiati@hotmail.com>
2024-05-16 09:13:23 +0100

    s8 l194 alternative to for loops: the forEach method

--- section8/arrays-01-starting-code 2/app.js
+++ section8/arrays-01-starting-code 2/app.js
@@ -44,34 +44,37 @@
 // const removedElements = hobbies.splice(-2, 2);
 // console.log(hobbies);
 
-const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
-// const storedResults = testResults.slice(2);
-const storedResults = testResults.concat([3.99, 2]);
-testResults.push(5.91);
+// const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
+// // const storedResults = testResults.slice(2);
+// const storedResults = testResults.concat([3.99, 2]);
+// testResults.push(5.91);
 
-console.log(storedResults, testResults);
+// console.log(storedResults, testResults);
 
-// console.log(testResults.indexOf(1.5));\\
-console.log(testResults.lastIndexOf(1.5));
+// // console.log(testResults.indexOf(1.5));\\
+// console.log(testResults.lastIndexOf(1.5));
 
-const personData = [{name: 'Romulo'}, {name: 'Isabela'}];
-console.log(personData.indexOf({name: 'Isabela'})); 
+// console.log('Is 10.99 included in this array', testResults.includes(10.99));
+// console.log('Is 10.99 included in this array', testResults.indexOf(10.99) !== -1);
 
-const isabela = personData.find((person, idx, persons) => {
-    return person.name = 'Isabela';
-});
+// const personData = [{name: 'Romulo'}, {name: 'Isabela'}];
+// console.log(personData.indexOf({name: 'Isabela'})); 
 
-console.log(isabela);
+// const isabela = personData.find((person, idx, persons) => {
+//     return person.name = 'Isabela';
+// });
 
-isabela.name = 'Anna';
+// // console.log(isabela);
 
-console.log(isabela, personData);
+// isabela.name = 'Anna';
 
-const annaIndex = personData.findIndex((person, idx, persons) => {
-    return person.name = 'Anna';
-});
+// console.log(isabela, personData);
 
-console.log(annaIndex);
+// const annaIndex = personData.findIndex((person, idx, persons) => {
+//     return person.name = 'Anna';
+// });
+
+// console.log(annaIndex);
 
 // the result of the 
 // indexOf() method when using objects, which are reference values, is 
@@ -79,3 +82,18 @@ console.log(annaIndex);
 // of indexOf() I passed another object, so the method could not find 
 // a matching object because both are different. There is another 
 // method that works for this case.
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+const taxAdjustedPrices = [];
+
+// for (const price of prices) {
+//     taxAdjustedPrices.push(price * (1 + tax));
+// }
+
+prices.forEach((price, idx, prices) => {
+    const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+    taxAdjustedPrices.push(priceObj);
+});
+
+console.log(taxAdjustedPrices);
