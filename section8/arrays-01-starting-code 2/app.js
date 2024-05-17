b3fd409757a7120e1d9c066813df6c305ab5b005
romuloabbiati <rhabbiati@hotmail.com>
2024-05-17 06:35:17 +0100

    s8 l195 transforming data with map

--- section8/arrays-01-starting-code 2/app.js
+++ section8/arrays-01-starting-code 2/app.js
@@ -83,17 +83,27 @@
 // a matching object because both are different. There is another 
 // method that works for this case.
 
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+// const taxAdjustedPrices = [];
+
+// // for (const price of prices) {
+// //     taxAdjustedPrices.push(price * (1 + tax));
+// // }
+
+// prices.forEach((price, idx, prices) => {
+//     const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+//     taxAdjustedPrices.push(priceObj);
+// });
+
+// console.log(taxAdjustedPrices);
+
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
-const taxAdjustedPrices = [];
 
-// for (const price of prices) {
-//     taxAdjustedPrices.push(price * (1 + tax));
-// }
-
-prices.forEach((price, idx, prices) => {
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
     const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
-    taxAdjustedPrices.push(priceObj);
+    return priceObj;
 });
 
-console.log(taxAdjustedPrices);
+// console.log(prices, taxAdjustedPrices);
