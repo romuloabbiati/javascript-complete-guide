fade6ec8714938a13f7bcef05712b82c1dffe697
romuloabbiati <rhabbiati@hotmail.com>
2024-05-21 21:51:26 +0100

    s8 l208 l209 understanding WeakSet and WeakMap

--- section8/arrays-01-starting-code 2/setsmaps.js
+++ section8/arrays-01-starting-code 2/setsmaps.js
@@ -8,25 +8,42 @@
 //     console.log(entry[0]);
 // }
 
-const person1 = {name: 'Romulo'};
-const person2 = {name: 'Isabela'};
+// const person1 = {name: 'Romulo'};
+// const person2 = {name: 'Isabela'};
 
-const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]]);
+// const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]]);
 
-personData.set(person2, [{date: 'two weeks ago', price: 100}]);
+// personData.set(person2, [{date: 'two weeks ago', price: 100}]);
+
+// console.log(personData);
+// console.log(personData.get(person1));
+
+// // for (const entry of personData.entries()) {
+// //     console.log(entry);
+// // }
+// // for (const [key, value] of personData.entries()) {
+// //     console.log(key, value);
+// // }
+// // for (const key of personData.keys()) {
+// //     console.log(key);
+// // }
+// for (const value of personData.values()) {
+//     console.log(value);
+// }
+
+let person = {name: 'Romulo'};
+const persons = new WeakSet();
+persons.add(person);
+
+// ... some operation
+// person = null;
+
+console.log(persons);
+
+const personData = new WeakMap();
+personData.set(person, 'Extra info!');
+
+person = null;
 
 console.log(personData);
-console.log(personData.get(person1));
 
-// for (const entry of personData.entries()) {
-//     console.log(entry);
-// }
-// for (const [key, value] of personData.entries()) {
-//     console.log(key, value);
-// }
-// for (const key of personData.keys()) {
-//     console.log(key);
-// }
-for (const value of personData.values()) {
-    console.log(value);
-}
