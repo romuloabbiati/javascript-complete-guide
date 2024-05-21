cc423040fcbd5481d739b31cfd956d561ab883ac
romuloabbiati <rhabbiati@hotmail.com>
2024-05-21 09:31:50 +0100

    s8 l206 working with maps

---
+++ section8/arrays-01-starting-code 2/setsmaps.js
@@ -0,0 +1,32 @@
+// const ids = new Set(['Hi', 'from', 'set']);
+// ids.add(2);
+// if(ids.has('Hi')) {
+//     ids.delete('Hi');
+// }
+
+// for (const entry of ids.entries()) {
+//     console.log(entry[0]);
+// }
+
+const person1 = {name: 'Romulo'};
+const person2 = {name: 'Isabela'};
+
+const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]]);
+
+personData.set(person2, [{date: 'two weeks ago', price: 100}]);
+
+console.log(personData);
+console.log(personData.get(person1));
+
+// for (const entry of personData.entries()) {
+//     console.log(entry);
+// }
+// for (const [key, value] of personData.entries()) {
+//     console.log(key, value);
+// }
+// for (const key of personData.keys()) {
+//     console.log(key);
+// }
+for (const value of personData.values()) {
+    console.log(value);
+}
