a0419626b519c1e92dc7c919dc5f02845a46d785
romuloabbiati <rhabbiati@hotmail.com>
2024-06-02 10:06:07 +0100

    s9 l237 getters and setters

--- section9/objects-01-starting-project/assets/scripts/objects.js
+++ section9/objects-01-starting-project/assets/scripts/objects.js
@@ -37,7 +37,7 @@ const renderMovies = (filter = "") => {
     // getFormattedtitle = getFormattedtitle.bind(movie);
     let text = getFormattedtitle.apply(movie) + ' - ';
     for (const key in info) {
-      if (key !== "title") {
+      if (key !== "title" && key !== '_title') {
         text = text + `${key}: ${info[key]}`;
       }
     }
@@ -52,7 +52,7 @@ const addMovieHandler = () => {
   const extraValue = document.getElementById("extra-value").value;
 
   if (
-    (title.trim() === "") |
+    // (title.trim() === "") |
     (extraName.trim() === "") |
     (extraValue.trim() === "")
   ) {
@@ -61,7 +61,17 @@ const addMovieHandler = () => {
 
   const newMovie = {
     info: {
-      title: title,
+      set title(val) {
+        if(val.trim() === '') {
+          this._title = 'DEFAULT';
+          return;
+        }
+        this._title = val;
+      },
+      // title: title,
+      get title() {
+        return this._title;
+      },
       [extraName]: extraValue,
     },
     id: Math.random().toString(),
@@ -71,11 +81,15 @@ const addMovieHandler = () => {
     }
   };
 
+  newMovie.info.title = title;
+  console.log(newMovie.info.title);
+
   movies.push(newMovie);
   renderMovies();
 };
 
 const searchMovieHandler = () => {
+  console.log(this);
   const filterTerm = document.getElementById("filter-title").value;
   renderMovies(filterTerm);
 };
