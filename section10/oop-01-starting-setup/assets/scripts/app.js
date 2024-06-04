1bffae190c0eb0ac070654c24f4929032e0e8c32
romuloabbiati <rhabbiati@hotmail.com>
2024-06-04 09:01:54 +0100

    s10 l244 working with constructor methods

--- section10/oop-01-starting-setup/assets/scripts/app.js
+++ section10/oop-01-starting-setup/assets/scripts/app.js
@@ -1,28 +1,31 @@
 class Product {
-  title = 'DEFAULT';
+  title = "DEFAULT";
   imgUrl;
-  price;
   description;
-}
+  price;
 
-console.log(new Product());
+  constructor(title, image, description, price) {
+    this.title = title;
+    this.imgUrl = image;
+    this.description = description;
+    this.price = price;
+  }
+}
 
 const productList = {
   products: [
-    {
-      title: "A pillow",
-      imgUrl:
-        "https://content.cylindo.com/api/v2/4980/products/20354CUSHIONS-65%20X%2065/frames/4/20354CUSHIONS-65%20X%2065.webp?size=1024&feature=COLOUR:347",
-      price: 19.99,
-      description: "A soft pillow",
-    },
-    {
-      title: "A carpet",
-      imgUrl:
-        "https://images.ctfassets.net/9y4fi2gkk4ov/47bITZF9JSc6RIMohq0qap/53db350cfd22912cb642cdb504a5a660/Cambria_Ruby_Tufted_Rug_5x7.jpg?fm=avif&w=1920&q=75",
-      price: 89.99,
-      description: "A carpet you might like - or not",
-    },
+    new Product(
+      "A pillow",
+      "https://content.cylindo.com/api/v2/4980/products/20354CUSHIONS-65%20X%2065/frames/4/20354CUSHIONS-65%20X%2065.webp?size=1024&feature=COLOUR:347",
+      "A soft pillow",
+      19.99
+    ),
+    new Product(
+      "A carpet",
+      "https://images.ctfassets.net/9y4fi2gkk4ov/47bITZF9JSc6RIMohq0qap/53db350cfd22912cb642cdb504a5a660/Cambria_Ruby_Tufted_Rug_5x7.jpg?fm=avif&w=1920&q=75",
+      "A carpet you might like - or not",
+      89.99
+    ),
   ],
   render() {
     const renderHook = document.getElementById("app");
