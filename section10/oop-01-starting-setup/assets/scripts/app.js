7280f2c6db130269f159539a7a6ae10f96bd8f38
romuloabbiati <rhabbiati@hotmail.com>
2024-06-06 08:31:45 +0100

    s10 l247 binding class methods and working with this

--- section10/oop-01-starting-setup/assets/scripts/app.js
+++ section10/oop-01-starting-setup/assets/scripts/app.js
@@ -12,8 +12,36 @@ class Product {
   }
 }
 
-const productList = {
-  products: [
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    console.log('Adding product to cart...');
+    console.log(this.product);
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+      prodEl.className = "product-item";
+      prodEl.innerHTML = `
+                <img src='${this.product.imgUrl}' alt='${this.product.title}' >
+                <div class='product-item__content'>
+                    <h2>${this.product.title}</h2>
+                    <h3>\$${this.product.price}</h3>
+                    <p>${this.product.description}</p>
+                    <button>Add to Cart</button>
+                </div>
+      `;
+      const addCartButton = prodEl.querySelector('button');
+      addCartButton.addEventListener('click', this.addToCart.bind(this));
+      return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
     new Product(
       "A pillow",
       "https://content.cylindo.com/api/v2/4980/products/20354CUSHIONS-65%20X%2065/frames/4/20354CUSHIONS-65%20X%2065.webp?size=1024&feature=COLOUR:347",
@@ -26,28 +54,24 @@ const productList = {
       "A carpet you might like - or not",
       89.99
     ),
-  ],
+  ]
+
+  constructor() {}
+
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
 
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-                <img src='${prod.imgUrl}' alt='${prod.title}' >
-                <div class='product-item__content'>
-                    <h2>${prod.title}</h2>
-                    <h3>\$${prod.price}</h3>
-                    <p>${prod.description}</p>
-                </div>
-            `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
 
     renderHook.append(prodList);
-  },
-};
+  }
+}
 
+const productList = new ProductList();
 productList.render();
