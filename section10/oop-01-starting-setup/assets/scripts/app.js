52054925ed6ae26f284f27b2cfc126dae53bc44a
romuloabbiati <rhabbiati@hotmail.com>
2024-06-11 21:07:40 +0100

    s10 l250 static method & properties

--- section10/oop-01-starting-setup/assets/scripts/app.js
+++ section10/oop-01-starting-setup/assets/scripts/app.js
@@ -17,7 +17,7 @@ class ShoppingCart {
 
   addProduct(product) {
     this.items.push(product);
-    this.totalOutput = `
+    this.totalOutput.innerHTML = `
       <h2>Total: \$${1}</h2>
     `;
   }
@@ -40,8 +40,7 @@ class ProductItem {
   }
 
   addToCart() {
-    console.log("Adding product to cart...");
-    console.log(this.product);
+    App.addProductToCart(this.product);
   }
 
   render() {
@@ -97,8 +96,8 @@ class Shop {
   render() {
     const renderHook = document.getElementById("app");
 
-    const cart = new ShoppingCart();
-    const cartEl = cart.render();
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
     const productList = new ProductList();
     const productListEl = productList.render();
 
@@ -107,5 +106,18 @@ class Shop {
   }
 }
 
-const shop = new Shop();
-shop.render();
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
